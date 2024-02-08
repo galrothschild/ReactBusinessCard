96ad38976e3c328cf5950313012958dbacaf30a7
galrothschild <131006606+galrothschild@users.noreply.github.com>
2024-02-08 06:57:47 +0200

    fixing schema typing in useForm

--- src/forms/hooks/useForm.ts
+++ src/forms/hooks/useForm.ts
@@ -1,24 +1,24 @@
-import Joi from "joi";
-import { useCallback, useState } from "react"
+import Joi, { ObjectSchema } from "joi";
+import { useCallback, useState } from "react";
 
-export const useForm = (initialForm: Record<string, string>, schema, handleSubmit) => {
+export const useForm = (initialForm: Record<string, string>, schema: ObjectSchema<Record<string, any>>, handleSubmit: Function) => {
     const [data, setData] = useState(initialForm);
     const [errors, setErrors] = useState({});
 
     const handleReset = useCallback(() => {
         setData(initialForm);
         setErrors({});
-    }, [initialForm])
+    }, [initialForm]);
     type propertyType = {
         name: string;
         value: string;
-    }
+    };
     const validateProperty = useCallback(({ name, value }: propertyType) => {
         const obj = { [name]: value };
-        const propertySchema = Joi.object({ [name]: schema[name] })
-        const { error } = propertySchema.validate(obj)
-        return error ? error.details[0].message : null
-    }, [schema])
+        const propertySchema = Joi.object({ [name]: schema[name as keyof typeof schema] });
+        const { error } = propertySchema.validate(obj);
+        return error ? error.details[0].message : null;
+    }, [schema]);
 
 
-}
+};
