ba82032573a945c3a55487248282a10067d4b39e
galrothschild <131006606+galrothschild@users.noreply.github.com>
2024-02-25 20:50:35 +0200

    Almost nothing

--- src/cards/components/card/MappedCards.tsx
+++ src/cards/components/card/MappedCards.tsx
@@ -4,43 +4,40 @@ import { ICard } from "../../models/CardModel";
 import CardComponent from "./CardComponent";
 
 interface Props {
-  cards: ICard[];
-  isPending: boolean;
+	cards: ICard[];
+	isPending: boolean;
 }
 
 const MappedCards: React.FC<Props> = ({ cards, isPending }) => {
-  if (isPending)
-    return (
-      <Box width="100%" sx={{ display: "grid", placeItems: "center", pt: 3 }}>
-        <CircularProgress />
-      </Box>
-    );
-  if (cards && !!cards.length) {
-    return (
-      <Grid container spacing={2} alignItems="center">
-        {cards.map((card, index) => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-            key={`${index}-card`}
-            sx={{ placeItems: "center", display: "grid" }}
-          >
-            <CardComponent card={card} />
-          </Grid>
-        ))}
-      </Grid>
-    ) as React.ReactElement<any, any> | null;
-  }
-  if (cards && !cards.length) {
-    return (
-      <Typography>It seems there are no cards to be displayed</Typography>
-    ) as React.ReactElement<any, any> | null;
-  } else {
-    return <Typography>Error occured</Typography>;
-  }
+	if (isPending)
+		return (
+			<Box width="100%" sx={{ display: "grid", placeItems: "center", pt: 3 }}>
+				<CircularProgress />
+			</Box>
+		);
+	if (cards && !!cards.length) {
+		return (
+			<Grid container spacing={2} alignItems="center">
+				{cards.map((card) => (
+					<Grid
+						item
+						xs={12}
+						sm={6}
+						md={4}
+						lg={3}
+						key={card._id}
+						sx={{ placeItems: "center", display: "grid" }}
+					>
+						<CardComponent card={card} />
+					</Grid>
+				))}
+			</Grid>
+		);
+	}
+	if (cards && !cards.length) {
+		return <Typography>It seems there are no cards to be displayed</Typography>;
+	}
+	return <Typography>Error occured</Typography>;
 };
 
 export default memo(MappedCards);
