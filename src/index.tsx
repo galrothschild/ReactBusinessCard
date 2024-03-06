b9c50a563676c590b9239ab614b35f9f82038334
galrothschild <131006606+galrothschild@users.noreply.github.com>
2024-03-06 14:52:33 +0200

    Moving to HashRouter to check if works better in Github

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
@@ -13,13 +13,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename="ReactBusinessCard">
+		<HashRouter>
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>
 					<App />
 				</PersistGate>
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 );
 reportWebVitals();
