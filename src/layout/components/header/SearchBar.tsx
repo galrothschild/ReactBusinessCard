9621709bd604452959d48a9d260452196b0e7f7b
galrothschild <131006606+galrothschild@users.noreply.github.com>
2024-03-06 15:40:04 +0200

    Remove unused imports in SearchBar component

--- src/layout/components/header/SearchBar.tsx
+++ src/layout/components/header/SearchBar.tsx
@@ -2,8 +2,8 @@ import { styled, alpha } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
-import { memo, useEffect } from "react";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { memo } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Search = styled("div")(({ theme }) => ({
 	position: "relative",
